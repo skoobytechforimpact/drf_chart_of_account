8d80d68c08484648d96d6d3bcdb0dcf0bd44f73b
Pritom Borogoria <pritom.saan@gmail.com>
2020-10-09 15:40:16 +0600

    Abstract class work has begun. Need to fix the Flake8 errors first

--- drf_chart_of_account/models.py
+++ drf_chart_of_account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class LayersBaseModel(models.Model):
+    """This is the abstract base class for all the layer models.
+
+    All other layer model classes will extend this class. Only difference is
+    the child layer class will override a foreign key relation key with it's
+    parent class
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=80, null=False, blank=False, verbose_name='Layer Name')
